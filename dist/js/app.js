b139b4878054acafa58deb9eaf3c7a83f6e0a63a
Aleksandr Morkovkin <morkovkin.a.s@neyvabank.ru>
2021-04-28 17:49:03 +0500

    settings modal window

--- dist/js/app.js
+++ dist/js/app.js
@@ -19,18 +19,41 @@ $('.fa-times').on('click', function() {
     $('.banner').slideUp(300)
 })
 
+// $(document).ready(function () {
+//     $('.header-wrp__phone-link').magnificPopup({
+//         items: {
+//             src: `<div id="popup" class="white-popup">
+//                         <form class="popup__form" action="#">
+//                             <div class="popup__form-name" ><input id="name" type="text" placeholder="Ваше имя" required/></div>
+//                             <div class="popup__form-tel"><input id="phone" type="text" placeholder="+9(999)99999999" required/></div>
+//                             <span>Удобное время для звонка</span>
+//                             <div class="popup__form-btn"><input type="button" value="Отправить"></div>
+//                         </form>
+//                     </div>`,
+//             type: 'inline'
+//         },
+//         preloader: false,
+//         modal: true
+//     });
+// })
+// $('.header-wrp__phone-link').click(function(){
+//     $.magnificPopup.close();
+//     $.magnificPopup.open({
+//       items: {
+//       src: $('#popup'),
+//         type: 'inline',
+//         }
+//     });
+// });
 $('.header-wrp__phone-link').magnificPopup({
-    items: {
-        src: `<div id="popup" class="white-popup">
-                    <form class="popup__form" action="#">
-                        <div class="popup__form-name" ><input id="name" type="text" placeholder="Ваше имя" required/></div>
-                        <div class="popup__form-tel"><input id="phone" type="text" placeholder="+9(999)99999999" required/></div>
-                        <span>Удобное время для звонка</span>
-                        <div><input type="popup__form-btn" value="Отправить"></div>
-                    </form>
-                </div>`,
-        type: 'inline'
-    }
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+        beforeOpen: function () {
+            this.st.mainClass = this.st.el.attr('data-effect');
+        }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
 
-$('#phone').mask('+9(999)99999999')
+$('#phone').mask('+9(999)9999999')
